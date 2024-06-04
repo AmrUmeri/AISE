8afc42c902c13baad9097ea83a4e89a4b1b2aa0f
d60134 <amr.umeri@d-fine.ch>
2024-06-05 01:37:23 +0200

    Add folder Project_2

---
+++ Project_2/Common.py
@@ -0,0 +1,323 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import torch.nn.functional as F
+from torch.optim import Adam
+import time
+import torch.nn as nn
+import torch.optim as optim
+
+torch.autograd.set_detect_anomaly(True)
+torch.manual_seed(128)
+
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+#ToDo: generalize the code..
+
+
+class NeuralNet(nn.Module):
+
+    def __init__(self, input_dimension, output_dimension, n_hidden_layers, neurons, retrain_seed):
+        super(NeuralNet, self).__init__()
+        # Number of input dimensions n
+        self.input_dimension = input_dimension
+        # Number of output dimensions m
+        self.output_dimension = output_dimension
+        # Number of neurons per layer
+        self.neurons = neurons
+        # Number of hidden layers
+        self.n_hidden_layers = n_hidden_layers
+        # Activation function
+        self.activation = nn.Tanh()#ReLU() #Tanh()
+
+        self.input_layer = nn.Linear(self.input_dimension, self.neurons)
+        self.hidden_layers = nn.ModuleList([nn.Linear(self.neurons, self.neurons) for _ in range(n_hidden_layers - 1)])
+        self.output_layer = nn.Linear(self.neurons, self.output_dimension)
+        self.retrain_seed = retrain_seed
+        # Random Seed for weight initialization
+        self.init_xavier()
+
+    def forward(self, x):
+        # The forward function performs the set of affine and non-linear transformations defining the network
+        # (see equation above)
+        x = self.activation(self.input_layer(x))
+        for k, l in enumerate(self.hidden_layers):
+            x = self.activation(l(x))
+        return self.output_layer(x)
+
+    def init_xavier(self):
+        torch.manual_seed(self.retrain_seed)
+
+        def init_weights(m):
+            if type(m) == nn.Linear and m.weight.requires_grad and m.bias.requires_grad:
+                g = nn.init.calculate_gain('tanh')
+                torch.nn.init.xavier_uniform_(m.weight, gain=g)
+                # torch.nn.init.xavier_normal_(m.weight, gain=g)
+                m.bias.data.fill_(0)
+
+        self.apply(init_weights)
+
+class NeuralNetReLu(nn.Module):
+
+    def __init__(self, input_dimension, output_dimension, n_hidden_layers, neurons, retrain_seed):
+        super(NeuralNetReLu, self).__init__()
+        # Number of input dimensions n
+        self.input_dimension = input_dimension
+        # Number of output dimensions m
+        self.output_dimension = output_dimension
+        # Number of neurons per layer
+        self.neurons = neurons
+        # Number of hidden layers
+        self.n_hidden_layers = n_hidden_layers
+        # Activation function
+        self.activation = nn.ReLU() 
+
+        self.input_layer = nn.Linear(self.input_dimension, self.neurons)
+        self.hidden_layers = nn.ModuleList([nn.Linear(self.neurons, self.neurons) for _ in range(n_hidden_layers - 1)])
+        self.output_layer = nn.Linear(self.neurons, self.output_dimension)
+        self.retrain_seed = retrain_seed
+        # Random Seed for weight initialization
+        self.init_xavier()
+
+    def forward(self, x):
+        # The forward function performs the set of affine and non-linear transformations defining the network
+        # (see equation above)
+        x = self.activation(self.input_layer(x))
+        for k, l in enumerate(self.hidden_layers):
+            x = self.activation(l(x))
+        return self.activation(self.output_layer(x))
+
+    def init_xavier(self):
+        torch.manual_seed(self.retrain_seed)
+
+        def init_weights(m):
+            if type(m) == nn.Linear and m.weight.requires_grad and m.bias.requires_grad:
+                g = nn.init.calculate_gain('tanh')
+                torch.nn.init.xavier_uniform_(m.weight, gain=g)
+                # torch.nn.init.xavier_normal_(m.weight, gain=g)
+                m.bias.data.fill_(0)
+
+        self.apply(init_weights)
+
+def activation(name):
+    if name in ['tanh', 'Tanh']:
+        return nn.Tanh()
+    elif name in ['relu', 'ReLU']:
+        return nn.ReLU(inplace=True)
+    elif name in ['lrelu', 'LReLU']:
+        return nn.LeakyReLU(inplace=True)
+    elif name in ['sigmoid', 'Sigmoid']:
+        return nn.Sigmoid()
+    elif name in ['softplus', 'Softplus']:
+        return nn.Softplus(beta=4)
+    elif name in ['celu', 'CeLU']:
+        return nn.CELU()
+    elif name in ['elu']:
+        return nn.ELU()
+    elif name in ['mish']:
+        return nn.Mish()
+    else:
+        raise ValueError('Unknown activation function')
+
+# Spectral (or Fourier) layer in 1d
+
+class SpectralConv1d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1):
+        super(SpectralConv1d, self).__init__()
+
+        """
+        1D Fourier layer. It does FFT, linear transform, and Inverse FFT.
+        """
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, dtype=torch.cfloat))
+
+    # Complex multiplication
+    def compl_mul1d(self, input, weights):
+        # (batch, in_channel, x ), (in_channel, out_channel, x) -> (batch, out_channel, x)
+        return torch.einsum("bix,iox->box", input, weights)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        # x.shape == [batch_size, in_channels, number of grid points]
+        # hint: use torch.fft library torch.fft.rfft
+        # use DFT to approximate the fourier transform
+
+        # Compute Fourier coefficients
+        x_ft = torch.fft.rfft(x)
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels, x.size(-1) // 2 + 1, device=x.device, dtype=torch.cfloat)
+        out_ft[:, :, :self.modes1] = self.compl_mul1d(x_ft[:, :, :self.modes1], self.weights1)
+
+        # Return to physical space
+        x = torch.fft.irfft(out_ft, n=x.size(-1))
+        return x
+
+class FNO1d(nn.Module):
+    def __init__(self, modes, width):
+        super(FNO1d, self).__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+
+        input: the solution of the initial condition and location (a(x), x)
+        input shape: (batchsize, x=s, c=2)
+        output: the solution of a later timestep
+        output shape: (batchsize, x=s, c=1)
+        """
+
+        self.modes1 = modes
+        self.width = width
+        self.padding = 1  # pad the domain if input is non-periodic
+        self.linear_p = nn.Linear(2, self.width)  # input channel is 2: (u0(x), x) --> GRID IS INCLUDED!
+
+        self.spect1 = SpectralConv1d(self.width, self.width, self.modes1)
+        self.spect2 = SpectralConv1d(self.width, self.width, self.modes1)
+        self.spect3 = SpectralConv1d(self.width, self.width, self.modes1)
+        self.lin0 = nn.Conv1d(self.width, self.width, 1)
+        self.lin1 = nn.Conv1d(self.width, self.width, 1)
+        self.lin2 = nn.Conv1d(self.width, self.width, 1)
+
+        self.linear_q = nn.Linear(self.width, 32)
+        self.output_layer = nn.Linear(32, 1)
+
+        self.activation = torch.nn.Tanh()
+
+    def fourier_layer(self, x, spectral_layer, conv_layer):
+        return self.activation(spectral_layer(x) + conv_layer(x))
+
+    def linear_layer(self, x, linear_transformation):
+        return self.activation(linear_transformation(x))
+
+    def forward(self, x):
+        # grid = self.get_grid(x.shape, x.device)
+        # x = torch.cat((x, grid), dim=-1)
+        x = self.linear_p(x)
+        x = x.permute(0, 2, 1)
+
+        x = F.pad(x, [0, self.padding])  # pad the domain if input is non-periodic
+
+        x = self.fourier_layer(x, self.spect1, self.lin0)
+        x = self.fourier_layer(x, self.spect2, self.lin1)
+        x = self.fourier_layer(x, self.spect3, self.lin2)
+
+        x = x[..., :-self.padding]  # pad the domain if input is non-periodic
+        x = x.permute(0, 2, 1)
+
+        x = self.linear_layer(x, self.linear_q)
+        x = self.output_layer(x)
+        return x
+
+
+################################################################
+#  2d fourier layer
+################################################################
+class SpectralConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1, modes2):
+        super(SpectralConv2d, self).__init__()
+
+        """
+        2D Fourier layer. It does FFT, linear transform, and Inverse FFT.
+        """
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1  # Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes2 = modes2
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+
+    # Complex multiplication
+    def compl_mul2d(self, input, weights):
+        # (batch, in_channel, x,y ), (in_channel, out_channel, x,y) -> (batch, out_channel, x,y)
+        return torch.einsum("bixy,ioxy->boxy", input, weights)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        # Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfft2(x)
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels, x.size(-2), x.size(-1) // 2 + 1, dtype=torch.cfloat, device=x.device)
+        out_ft[:, :, :self.modes1, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
+
+        # Return to physical space
+        x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
+        return x
+
+class FNO2d(nn.Module):
+    def __init__(self, fno_architecture, device=None, padding_frac=1 / 4):
+        super(FNO2d, self).__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+
+        input: the solution of the coefficient function and locations (a(x, y), x, y)
+        input shape: (batchsize, x=s, y=s, c=3)
+        output: the solution
+        output shape: (batchsize, x=s, y=s, c=1)
+        """
+        self.modes1 = fno_architecture["modes"]
+        self.modes2 = fno_architecture["modes"]
+        self.width = fno_architecture["width"]
+        self.n_layers = fno_architecture["n_layers"]
+        self.retrain_fno = fno_architecture["retrain_fno"]
+
+        torch.manual_seed(self.retrain_fno)
+        # self.padding = 9 # pad the domain if input is non-periodic
+        self.padding_frac = padding_frac
+        self.fc0 = nn.Linear(3, self.width)  # input channel is 3: (a(x, y), x, y)
+
+        self.conv_list = nn.ModuleList(
+            [nn.Conv2d(self.width, self.width, 1) for _ in range(self.n_layers)])
+        self.spectral_list = nn.ModuleList(
+            [SpectralConv2d(self.width, self.width, self.modes1, self.modes2) for _ in range(self.n_layers)])
+
+        self.fc1 = nn.Linear(self.width, 128)
+        self.fc2 = nn.Linear(128, 1)
+
+        self.to(device)
+
+    def forward(self, x):
+        x = self.fc0(x)
+        x = x.permute(0, 3, 1, 2)
+
+        x1_padding = int(round(x.shape[-1] * self.padding_frac))
+        x2_padding = int(round(x.shape[-2] * self.padding_frac))
+        x = F.pad(x, [0, x1_padding, 0, x2_padding])
+
+        for k, (s, c) in enumerate(zip(self.spectral_list, self.conv_list)):
+
+            x1 = s(x)
+            x2 = c(x)
+            x = x1 + x2
+            if k != self.n_layers - 1:
+                x = F.gelu(x)
+        x = x[..., :-x1_padding, :-x2_padding]
+
+        x = x.permute(0, 2, 3, 1)
+        x = self.fc1(x)
+        x = F.gelu(x)
+        x = self.fc2(x)
+        return x
+
+
